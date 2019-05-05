9d7c8bdb00c616ccb9e8133da23037d7b1b544d1
Bin Wang <jerry.binwang@gmail.com>
2019-05-05 16:35:13 -0400

    Implement packet capture

---
+++ trace-collector/config.py
@@ -0,0 +1,9 @@
+DEBUG = True
+
+PLAYBACK_TEMPLATE_FOLDER = "templates/"
+PLAYBACK_TEMPLATE_FILE = "stream.html.j2"
+PLAYBACK_RENDERED_FILE = "stream.html"
+
+RESULTS_FOLDER = "results/"
+MANIFEST_FILE = "manifest.json"
+PACKET_CAPTURE_FILE = "packets.pcap"
--- trace-collector/run.py
+++ trace-collector/run.py
@@ -1,50 +1,93 @@
+import datetime
+import os
+import subprocess
+from yaml import dump
 from selenium import webdriver
 
-TEMPLATE_FOLDER = "templates/"
-TEMPLATE_FILE = "stream.html.j2"
+import config
 
 
-def generate_video_playback_html(vidoe_id, output_file):
-    from jinja2 import Environment, FileSystemLoader
+class ExperimentRunner():
+    def __init__(self):
+        self.manifest = {}
 
-    env = Environment(loader=FileSystemLoader(TEMPLATE_FOLDER))
-    template = env.get_template(TEMPLATE_FILE)
-    with open(output_file, "w") as fout:
-        print(template.render(video_id=vidoe_id), file=fout)
+    def run(self):
+        self.select_video_id()
+        self.generate_playback_file()
+        self.set_network_condition()
+        self.prepare_results_folder()
+        self.start_packet_capture()
+        self.playback()
+        self.stop_packet_capture()
+        self.record_results()
 
+    def select_video_id(self):
+        self.manifest["video_id"] = "LzD79LJPNUE"
 
-def set_network_condition():
-    pass
+    def generate_playback_file(self):
+        from jinja2 import Environment, FileSystemLoader
+        env = Environment(loader=FileSystemLoader(config.PLAYBACK_TEMPLATE_FOLDER))
+        template = env.get_template(config.PLAYBACK_TEMPLATE_FILE)
+        with open(config.PLAYBACK_RENDERED_FILE, "w") as fout:
+            print(template.render(video_id=self.manifest["video_id"]), file=fout)
 
+    @staticmethod
+    def set_network_condition():
+        pass
 
-def playback(file):
-    from pathlib import Path
-    from time import sleep
+    @staticmethod
+    def prepare_results_folder():
+        import shutil
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
+        if os.path.exists(config.RESULTS_FOLDER):
+            shutil.rmtree(config.RESULTS_FOLDER)
+        os.mkdir(config.RESULTS_FOLDER)
 
-    uri = Path(file).absolute().as_uri()
-    driver.get(uri)
+    def playback(self):
+        from pathlib import Path
+        from time import sleep
 
-    # print(driver.page_source)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
 
-    driver.find_element_by_id("player").click()
+        uri = Path(config.PLAYBACK_RENDERED_FILE).absolute().as_uri()
+        driver.get(uri)
 
-    while True:
-        playback_status = driver.execute_script("return player.getPlayerState()")
-        playback_time = driver.execute_script("return player.getCurrentTime()")
-        playback_quality = driver.execute_script("return player.getPlaybackQuality()")
-        print("playback time:{}, status:{}, rate: {}".format(playback_time, playback_status, playback_quality))
-        sleep(1)
+        self.manifest["start_time"] = datetime.datetime.utcnow()
+        driver.find_element_by_id("player").click()
+        while True:
+            playback_status = driver.execute_script("return player.getPlayerState()")
+            if playback_status == 0:  # if playback has finished
+                break
 
-    driver.close()
+            if config.DEBUG:
+                playback_time = driver.execute_script("return player.getCurrentTime()")
+                playback_quality = driver.execute_script("return player.getPlaybackQuality()")
+                print("Playback time: {playback_time}, status: {playback_status}, quality: {playback_quality}"
+                      .format(playback_time=playback_time,
+                              playback_status=playback_status,
+                              playback_quality=playback_quality))
+
+            sleep(5)  # if playback has not finished, sleep for 5 seconds and then check again
+
+        self.manifest["end_time"] = datetime.datetime.utcnow()
+        driver.close()
+
+    def record_results(self):
+        with open(os.path.join(config.RESULTS_FOLDER, config.MANIFEST_FILE), "w") as fout:
+            dump(self.manifest, stream=fout)
+
+    @staticmethod
+    def start_packet_capture():
+        packet_capture_file = os.path.join(config.RESULTS_FOLDER, config.PACKET_CAPTURE_FILE)
+        subprocess.Popen(["sudo", "tcpdump", "-U", "-w", packet_capture_file])
+
+    @staticmethod
+    def stop_packet_capture():
+        subprocess.check_call(["sudo", "pkill", "tcpdump"])
 
 
 if __name__ == "__main__":
-    video_id = "TcMBFSGVi1c"
-    playback_file = "stream.html"
-    generate_video_playback_html(video_id, playback_file)
-    set_network_condition()
-    playback(playback_file)
+    runner = ExperimentRunner()
+    runner.run()
