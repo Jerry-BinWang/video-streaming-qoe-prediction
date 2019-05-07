f953ac6fb92718babb6e0e90519b7b621d49bbb4
Bin Wang <jerry.binwang@gmail.com>
2019-05-06 22:49:42 -0400

    Make network worse to prevent data imbalance

--- trace-collector/comcast.py
+++ trace-collector/comcast.py
@@ -16,7 +16,7 @@ class Comcast:
     CORRUPT_LOWER_BOUND = 0.1       # 0.1%
     CORRUPT_UPPER_BOUND = 2         # 2%
     REORDERING_LOWER_BOUND = 0.1    # 0.1%
-    REORDERING_UPPER_BOUND = 10     # 10%
+    REORDERING_UPPER_BOUND = 20     # 10%
 
     def __init__(self):
         self._rng = random.Random()
@@ -27,13 +27,13 @@ class Comcast:
         self.corrupt_rate = None
         self.reordering_rate = None
 
-    def coin_toss(self, true_prob = 0.5):
+    def coin_toss(self, true_prob=0.5):
         if self._rng.uniform(0, 1) <= true_prob:
             return True
         else:
             return False
 
-    def beta_random_range(self,alpha, beta, lower, upper):
+    def beta_random_range(self, alpha, beta, lower, upper):
         return self._rng.betavariate(alpha=alpha, beta=beta) * (upper - lower) + lower
 
     def generate_random_network_condition(self):
@@ -45,38 +45,35 @@ class Comcast:
 
         # random generate latency in 30 - 1000 ms
         if self.coin_toss():
-            self.latency = self.beta_random_range(alpha=1, beta=3,
-                                                  lower=self.LATENCY_LOWER_BOUND, upper=self.LATENCY_UPPER_BOUND)
+            self.latency = self._rng.uniform(self.LATENCY_LOWER_BOUND, self.LATENCY_UPPER_BOUND)
             self.latency = int(self.latency)
-            self.jitter = self.beta_random_range(alpha=1, beta=3,
-                                                 lower=self.JITTER_LOWER_BOUND, upper=self.JITTER_UPPER_BOUND)
+            self.jitter = self._rng.uniform(self.JITTER_LOWER_BOUND, self.JITTER_UPPER_BOUND)
             self.jitter = int(self.jitter * self.latency)
         else:
             self.latency = None
 
         # random generate packet loss
-        if self.coin_toss(true_prob=0.3):
-            self.loss_rate = self.beta_random_range(alpha=0.7, beta=3,
-                                                    lower=self.LOSS_LOWER_BOUND, upper=self.LOSS_UPPER_BOUND)
+        if self.coin_toss():
+            self.loss_rate = self._rng.uniform(self.LOSS_LOWER_BOUND, self.LOSS_UPPER_BOUND)
         else:
             self.loss_rate = None
 
         # random generate corrupt rate
-        if self.coin_toss(true_prob=0.1):
-            self.corrupt_rate = self.beta_random_range(alpha=0.7, beta=3,
+        if self.coin_toss():
+            self.corrupt_rate = self.beta_random_range(alpha=1, beta=2,
                                                        lower=self.CORRUPT_LOWER_BOUND, upper=self.CORRUPT_UPPER_BOUND)
         else:
             self.corrupt_rate = None
 
         # random generate reordering rate
-        if self.coin_toss(true_prob=0.1) and self.latency is not None:  # reordering must be used with latency
-            self.reordering_rate = self.beta_random_range(alpha=0.7, beta=3,
+        if self.coin_toss() and self.latency is not None:  # reordering must be used with latency
+            self.reordering_rate = self.beta_random_range(alpha=1, beta=3,
                                                           lower=self.REORDERING_LOWER_BOUND,
                                                           upper=self.REORDERING_UPPER_BOUND)
         else:
             self.reordering_rate = None
 
-    def apply(self, dry_run: bool=False):
+    def apply(self, dry_run: bool = False):
         cmd = ["tcset", config.NIC]
 
         if self.bandwidth is not None:
